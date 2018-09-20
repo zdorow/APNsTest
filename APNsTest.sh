f540223d3a146da213034e1b33e995460a85cdbe
Zach Dorow <zdorow@me.com>
2018-09-20 13:28:09 -0500

    Initial Upload

---
+++ APNsTest.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ nc -z 5-courier.sandbox.push.apple.com 5223
+ 
+ nc -z 5-courier.sandbox.push.apple.com 443
+ 
+ nc -z gateway.sandbox.push.apple.com 2195
+ 
+ nc -z gateway.sandbox.push.apple.com 2196
