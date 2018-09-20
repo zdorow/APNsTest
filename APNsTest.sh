0c45aeea918ffec1521b35346270a114b2e79d52
Zach Dorow <zdorow@me.com>
2018-09-20 13:45:12 -0500

    Added logic

--- APNsTest.sh
+++ APNsTest.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
- nc -z 5-courier.sandbox.push.apple.com 5223
- 
- nc -z 5-courier.sandbox.push.apple.com 443
- 
- nc -z gateway.sandbox.push.apple.com 2195
- 
- nc -z gateway.sandbox.push.apple.com 2196
+nc -z 5-courier.sandbox.push.apple.com 5223
+if [[ "$?" -ne 0 ]]; then
+    echo "5223 needs to be opened to Apple's 17/8 block"
+fi
+
+nc -z 5-courier.sandbox.push.apple.com 443
+if [[ "$?" -ne 0 ]]; then
+    echo "443 needs to be opened to Apple's 17/8 block"
+fi 
+
+nc -z gateway.sandbox.push.apple.com 2195
+if [[ "$?" -ne 0 ]]; then
+    echo "2195 needs to be opened to Apple's 17/8 block"
+fi
+
+nc -z gateway.sandbox.push.apple.com 2196
+if [[ "$?" -ne 0 ]]; then
+    echo "2196 needs to be opened to Apple's 17/8 block"
+fi
